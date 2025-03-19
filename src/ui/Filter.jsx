bb246fe3a331e97a409738c5029af988488c736c
Prabhjot Singh <prabhjotsingh394@gmail.com>
2025-03-19 09:59:31 +0530

    Filtering cabins

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -1,3 +1,4 @@
+import { useSearchParams } from "react-router-dom";
 import styled, { css } from "styled-components";
 
 const StyledFilter = styled.div`
@@ -33,3 +34,29 @@ const FilterButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
+
+function Filter({ filterField, options }) {
+  const [searchparams, setSearchParams] = useSearchParams();
+  const currentFilter = searchparams.get(filterField) || options.at(0).value;
+
+  function handleClick(value) {
+    searchparams.set(filterField, value);
+    setSearchParams(searchparams);
+  }
+
+  return (
+    <StyledFilter>
+      {options.map((option) => (
+        <FilterButton
+          key={option.value}
+          onClick={() => handleClick(option.value)}
+          active={option.value === currentFilter}
+        >
+          {option.label}
+        </FilterButton>
+      ))}
+    </StyledFilter>
+  );
+}
+
+export default Filter;
