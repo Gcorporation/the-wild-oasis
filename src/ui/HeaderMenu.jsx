e842520204b462e9d602019e7f6b7aac32e5fbab
Prabhjot Singh <prabhjotsingh394@gmail.com>
2025-04-06 13:44:28 +0530

    Implementing Dark mode with CSS Variables

--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -3,6 +3,7 @@ import Logout from "../features/authentication/Logout";
 import ButtonIcon from "./ButtonIcon";
 import { HiOutlineUser } from "react-icons/hi2";
 import { useNavigate } from "react-router-dom";
+import DarkModeTrigger from "./DarkModeToggle";
 
 const StyledheaderMenu = styled.ul`
   display: flex;
@@ -17,6 +18,9 @@ function HeaderMenu() {
         <ButtonIcon onClick={() => navigate("/account")}>
           <HiOutlineUser />
         </ButtonIcon>
+      </li>
+      <li>
+        <DarkModeTrigger />
       </li>
       <li>
         <Logout />
